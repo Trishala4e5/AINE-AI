a00e05c88fb4cb3afd118392e70522dbcd853d86
Trishala4e5 <79440974+Trishala4e5@users.noreply.github.com>
2021-07-06 13:49:15 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaDigitalOcean } from 'react-icons/fa';
+import { BsPeopleCircle } from 'react-icons/bs';
+import { BiNotepad } from 'react-icons/bi';
+import { FiSettings } from 'react-icons/fi';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="row">
+      <div class="column1">
+        <div className="search" >
+          <FaDigitalOcean size={50} onClick=""/> 
+          <p></p>
+        </div>
+        <div className="students" >
+          <BsPeopleCircle size={50} onClick=""/>
+          <p>Students</p>
+        </div>
+        <div className="notepad" >
+            <button onClick="myfunction()">
+                <BiNotepad size={50} />
+                <p >Lesson Plan</p>
+            </button>
+        </div>
+        <div className="settings" >
+          <FiSettings size={50} onClick=""/>
+          <p>Settings</p>
+        </div>                  
+      </div> 
+      <div class="column2">
+        <p>videos</p>
+        <input type="text" placeholder="Insert URL here " id="inputId" size="50"></input>
+        <input type="file" id="img" name="img" accept="video/*"></input>
+      </div>
     </div>
+    
+    
   );
 }
 
